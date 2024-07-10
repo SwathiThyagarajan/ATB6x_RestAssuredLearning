018eeef670d137f543b7c103282c8983a1854122
SwathiThyagarajan <swathi.maverick9919@gmail.com>
2024-07-10 21:10:43 +0530

    Initial commit

---
+++ src/test/java/com/thetestingacademy/ex_RA01/Lab260.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy.ex_RA01;
+
+import io.restassured.RestAssured;
+
+public class Lab260 {
+    public static void main(String[] args) {
+
+        // Creating Rest Assured Hello World Program
+
+        // Rest Assured is a java domain specific language
+        // it will help u write the automated tests for Restful APIs
+        // you can make any http request like post put get delete etc. and also validate the response
+
+        // Rest Assured follows something known as given when then.
+        // given when then is a style for representing test case
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when().get().then().statusCode(201);
+    }
+}
